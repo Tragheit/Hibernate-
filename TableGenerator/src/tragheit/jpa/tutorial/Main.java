7925f144739cb4a8c679f5cd0a376e93e9fde65b
rrble <rrble@192.168.1.20>
2018-07-23 00:34:04 +0200

    Dziedziczenie Single Table

--- TableGenerator/src/tragheit/jpa/tutorial/Main.java
+++ TableGenerator/src/tragheit/jpa/tutorial/Main.java
@@ -4,7 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import tragheit.jpa.tutorial.domain.Employee;
+import tragheit.jpa.tutorial.domain.Profesor;
+import tragheit.jpa.tutorial.domain.Student;
 
 public class Main {
 	private static EntityManagerFactory entityManagerFactory;
@@ -13,26 +14,20 @@ public class Main {
 	public static void main(String[] args) {
 		entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");
 		entityManager = entityManagerFactory.createEntityManager();
-
-		Employee employee1 = new Employee();
-		employee1.setFirstName("Jan");
-		employee1.setLastName("Nowak");
-		employee1.setSalary(3333.3);
 		
-		Employee employee2 = new Employee();
-		employee2.setFirstName("Robert");
-		employee2.setLastName("Bednarek");
-		employee2.setSalary(4444.4);
+		Profesor profesor = new Profesor();
+		profesor.setFirstName("Jan");
+		profesor.setLastName("Nowak");
+		profesor.setSalary(7563.0);
 		
-		Employee employee3 = new Employee();
-		employee3.setFirstName("Damian");
-		employee3.setLastName("Stach");
-		employee3.setSalary(5555.5);
+		Student student = new Student();
+		student.setFirstName("Marek");
+		student.setLastName("Kowalski");
+		student.setAverageGrade(4.75);
 		
 		entityManager.getTransaction().begin();
-		entityManager.persist(employee1);
-		entityManager.persist(employee2);
-		entityManager.persist(employee3);
+		entityManager.persist(profesor);
+		entityManager.persist(student);
 		entityManager.getTransaction().commit();
 			
 		entityManager.close();
---
+++ TableGenerator/src/tragheit/jpa/tutorial/domain/Profesor.java
@@ -0,0 +1,20 @@
+package tragheit.jpa.tutorial.domain;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("profesorek")
+
+public class Profesor extends Person{
+	private double salary;
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+}
